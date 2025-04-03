dd71bc5c9799e38420261713a47398ee4d73e355
Brendha Michels Ramos <brendha.ramos@getconnect.com.br>
2025-04-02 22:57:06 -0300

    fix dos reports

--- cypress/pages/ConsultarEventoPage.js
+++ cypress/pages/ConsultarEventoPage.js
@@ -1,16 +1,20 @@
 class ConsultarEventoPage {
     buscarProduto(termo) {
         cy.get('#search-input-field').should('be.visible').type(`${termo}{enter}`);
+        cy.wait(2000);
+        cy.screenshot('03_busca_termo', { capture: 'viewport' }); 
     }
 
-    aplicarFiltro(nomeFiltro, valorFiltro) {
-        cy.get(`input[name="${nomeFiltro}"][value="${valorFiltro}"]`).check();
+    aplicarFiltro(nome, valor) {
+        cy.get(`input[name="${nome}"][value="${valor}"]`).check();
+        cy.wait(2000);
+        cy.screenshot(`04_aplicar_filtro_${valor}`, { capture: 'viewport' }); 
     }
 
-    verificarURLContem(valorEsperado) {
-        cy.url().should('include', valorEsperado);
+    verificarURLContem(texto) {
+        cy.url().should('include', texto);
+        cy.screenshot(`05_validacao_${texto.replace(/[^a-zA-Z0-9]/g, '_')}`, { capture: 'viewport' }); 
     }
-
 }
 
 export default new ConsultarEventoPage();
--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -1,24 +1,25 @@
 class LoginPage {
     visitarPagina() {
         cy.visit('/');
+        cy.wait(2000); 
+        cy.screenshot('00_visitar_pagina', { capture: 'viewport' }); 
     }
-
     aceitarCookiesEFecharPopups() {
         cy.get('#onetrust-accept-btn-handler', { timeout: 10000 }).should('be.visible').click();
         cy.get('.gcap-close', { timeout: 10000 }).should('be.visible').click();
         cy.get('.jss107 > .MuiTypography-root', { timeout: 10000 }).should('be.visible').click();
+        cy.screenshot('01_pagina_de_login', { capture: 'viewport' });
     }
-
     fazerLogin(usuario, senha) {
         cy.origin('https://accounts.superbid.net', { args: { usuario, senha } }, ({ usuario, senha }) => {
             cy.get('input[name="login"]').should('be.visible').type(usuario);
             cy.get('input[name="password"]').should('be.visible').type(senha);
             cy.get('.btn-login').should('be.visible').click();
-        });
+        });        
     }
-
     verificarLoginComSucesso() {
         cy.get('.false > .MuiTypography-root').should('be.visible');
+        cy.screenshot('02_usuario_logado', { capture: 'viewport' });
     }
 }
 
